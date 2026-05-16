2c1b7e9f89077d73b9af8f9c0e02f60f5f142dc3
Shamil Rakhimov <cwxpsh@gmail.com>
2026-05-16 12:24:21 +0300

    Merge branch 'dev/rakhimovs-codesend'

--- internal/features/users/repository/postgres/patch_user.go
+++ internal/features/users/repository/postgres/patch_user.go
@@ -15,7 +15,7 @@ func (r *UserRepository) PatchUser(ctx context.Context,
 			UPDATE trackerapp.users 
 			SET full_name=$1, email=$2, phone_number=$3
 			WHERE id=$4
-			RETURNING id, full_name, email, phone_number, password, time_add, role, is_verified;`
+			RETURNING id, full_name, email, phone_number, password, time_add, role, is_verified, description;`
 
 	row := r.pool.QueryRow(ctx,
 		query,
@@ -34,7 +34,8 @@ func (r *UserRepository) PatchUser(ctx context.Context,
 		&UserUpdated.Password,
 		&UserUpdated.Time_add,
 		&UserUpdated.Role,
-		&UserUpdated.Is_verified)
+		&UserUpdated.Is_verified,
+		&UserUpdated.Description)
 	if err != nil {
 		return core_domain.User{}, fmt.Errorf("failed to update user: %w", err)
 	}
